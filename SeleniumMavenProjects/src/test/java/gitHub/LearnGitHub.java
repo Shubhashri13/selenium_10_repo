3a5bb5a4d1a006a1f9dce383f456b9f31021ad63
Pawan <Pawan@LAPTOP-L8DJVMDE>
2025-02-01 10:52:29 +0530

    GitHub laerning

---
+++ SeleniumMavenProjects/src/test/java/gitHub/LearnGitHub.java
@@ -0,0 +1,14 @@
+package gitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LearnGitHub 
+{
+	@Test
+	public void git()
+	{
+		Reporter.log("GitHub laerning",true);
+	}
+
+}
